aaba3a39c7ccb0f40c45193a18f4e02adcad843e
Aeonss <30298077+Aeonss@users.noreply.github.com>
2023-03-29 21:10:59 -0400

    Transfered recipe API to edamam

--- flask/server.py
+++ flask/server.py
@@ -12,49 +12,28 @@ def home():
 @app.route("/recipes/<query>", methods=['GET'])
 def search(query):
     
-    url = "https://www.themealdb.com/api/json/v1/1/search.php?s=" + query
+    url = f"https://api.edamam.com/api/recipes/v2?type=any&q={query}&app_id=0f91a740&app_key=cfe00546378229eb14793ea770992f82"
     soup = BeautifulSoup(requests.get(url).text, "html.parser")
     
-    recipes = []
+    json_recipes = []
+    recipes = json.loads(soup.text)['hits']
 
-    meals = json.loads(soup.text)
-    for recipe in meals['meals']:
+    for recipe in recipes:
+        recipe = recipe["recipe"]
         data = {}
-        data['id'] = recipe["idMeal"]
-        data['name'] = recipe["strMeal"]
-        data['sourceUrl'] = recipe["strSource"]
-        data['imageUrl'] = recipe["strMealThumb"]
-        data['totalMinutes'] = ""
+        data['id'] = recipe["uri"]
+        data['name'] = recipe["label"]
+        data['sourceUrl'] = recipe["url"]
+        data['imageUrl'] = recipe["image"]
+        data['totalMinutes'] = recipe["totalTime"]
         data['totalCost'] =  ""
-        data['numberServings'] = ""
-        
-        ingredients = []
-        ingredients.append(recipe["strIngredient1"])
-        ingredients.append(recipe["strIngredient2"])
-        ingredients.append(recipe["strIngredient3"])
-        ingredients.append(recipe["strIngredient4"])
-        ingredients.append(recipe["strIngredient5"])
-        ingredients.append(recipe["strIngredient6"])
-        ingredients.append(recipe["strIngredient7"])
-        ingredients.append(recipe["strIngredient8"])
-        ingredients.append(recipe["strIngredient9"])
-        ingredients.append(recipe["strIngredient10"])
-        ingredients.append(recipe["strIngredient11"])
-        ingredients.append(recipe["strIngredient12"])
-        ingredients.append(recipe["strIngredient13"])
-        ingredients.append(recipe["strIngredient14"])
-        ingredients.append(recipe["strIngredient15"])
-        ingredients.append(recipe["strIngredient16"])
-        ingredients.append(recipe["strIngredient17"])
-        ingredients.append(recipe["strIngredient18"])
-        ingredients.append(recipe["strIngredient19"])
-        ingredients.append(recipe["strIngredient20"])
-        
-        data['ingredients'] = ingredients
-        data['instructions'] = recipe["strInstructions"]
-        recipes.append(data)
-        
-    return jsonify(recipes)
+        data['numberServings'] = recipe["yield"]
+        data['ingredients'] = recipe["ingredientLines"]
+        data['instructions'] = "Instructions on source URL"
+
+        json_recipes.append(data)
+    
+    return jsonify(json_recipes)
 
 if __name__ == "__main__":
     app.run()
--- test/test.py
+++ test/test.py
@@ -1,47 +1,12 @@
 from bs4 import BeautifulSoup
 import json, requests
 
-url = "https://www.themealdb.com/api/json/v1/1/search.php?s=rice"
+url = "https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=0f91a740&app_key=cfe00546378229eb14793ea770992f82"
 soup = BeautifulSoup(requests.get(url).text, "html.parser")
-soup.text
 
-recipes = []
+json_recipes = []
 
-meals = json.loads(soup.text)
-for recipe in meals['meals']:
-        data = {}
-        data['id'] = recipe["idMeal"]
-        data['name'] = recipe["strMeal"]
-        data['sourceUrl'] = recipe["strSource"]
-        data['imageUrl'] = recipe["strMealThumb"]
-        data['totalMinutes'] = ""
-        data['totalCost'] =  ""
-        data['numberServings'] = ""
-        
-        ingredients = []
-        ingredients.append(recipe["strIngredient1"])
-        ingredients.append(recipe["strIngredient2"])
-        ingredients.append(recipe["strIngredient3"])
-        ingredients.append(recipe["strIngredient4"])
-        ingredients.append(recipe["strIngredient5"])
-        ingredients.append(recipe["strIngredient6"])
-        ingredients.append(recipe["strIngredient7"])
-        ingredients.append(recipe["strIngredient8"])
-        ingredients.append(recipe["strIngredient9"])
-        ingredients.append(recipe["strIngredient10"])
-        ingredients.append(recipe["strIngredient11"])
-        ingredients.append(recipe["strIngredient12"])
-        ingredients.append(recipe["strIngredient13"])
-        ingredients.append(recipe["strIngredient14"])
-        ingredients.append(recipe["strIngredient15"])
-        ingredients.append(recipe["strIngredient16"])
-        ingredients.append(recipe["strIngredient17"])
-        ingredients.append(recipe["strIngredient18"])
-        ingredients.append(recipe["strIngredient19"])
-        ingredients.append(recipe["strIngredient20"])
-        
-        data['ingredients'] = ingredients
-        data['instructions'] = recipe["strInstructions"]
-        recipes.append(data)
+recipes = json.loads(soup.text)['hits']
+
     
-print(recipes)
+print(len(recipes))
