d3b9d1e16916b8f9a85967b366ec9821b68b5660
Pranav Wadhwa <pranavwadhwa@outlook.com>
2023-03-30 13:23:37 -0400

    Working network requests for /recipes

--- ChefMeUp/views/RecipesView.tsx
+++ ChefMeUp/views/RecipesView.tsx
@@ -6,20 +6,20 @@ import {
   StyleSheet,
   FlatList,
   TouchableOpacity,
+  Image,
+  TextInput,
+  Keyboard,
+  ActivityIndicator,
 } from 'react-native';
 import {Recipe} from '../model/Recipe';
 import sampleRecipes from '../model/sampleRecipes';
 import {useState} from 'react';
-import {TextInput} from 'react-native-paper';
 import {RecipesViewProps} from './RecipeStackParams';
+import Network from '../network/network';
 
 const formatter = new Intl.NumberFormat('en-US', {
   style: 'currency',
   currency: 'USD',
-
-  // These options are needed to round to whole numbers if that's what you want.
-  //minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
-  //maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
 });
 
 const RecipeCard = (recipe: Recipe, onClick: () => void) => (
@@ -31,7 +31,7 @@ const RecipeCard = (recipe: Recipe, onClick: () => void) => (
         <View style={styles.recipeInfo}>
           <Text style={styles.recipeName}>{recipe.name}</Text>
           <Text style={styles.recipeDetails}>
-            {recipe.totalMinutes} min • $
+            {recipe.totalMinutes} min •
             {formatter.format(recipe.totalCost / recipe.numberServings)}/serving
           </Text>
         </View>
@@ -40,8 +40,34 @@ const RecipeCard = (recipe: Recipe, onClick: () => void) => (
   </TouchableOpacity>
 );
 
+const SearchBox = (
+  updateSearch: (_: string) => void,
+  search: string,
+  performSearch: () => void,
+) => (
+  <View style={styles.searchBar}>
+    <TextInput
+      onChangeText={updateSearch}
+      value={search}
+      placeholder="Search for a recipe..."
+      placeholderTextColor="#555"
+      style={styles.searchInput}
+      returnKeyType="search"
+      onSubmitEditing={performSearch}></TextInput>
+    <TouchableOpacity onPress={performSearch} style={styles.searchIcon}>
+      <Image
+        source={require('../assets/bxs-search.png')}
+        style={{tintColor: '#fff', width: 32, height: 32}}
+      />
+    </TouchableOpacity>
+  </View>
+);
+
 const RecipesView = ({route, navigation}: RecipesViewProps) => {
   const [search, setSearch] = useState('');
+  const [recipes, setRecipes] = useState<Recipe[]>([]);
+  const [isSearching, setIsSearching] = useState(false);
+  const [message, setMessage] = useState('Search for a recipe to get started!');
   const updateSearch = (text: string): void => {
     setSearch(text);
   };
@@ -50,20 +76,44 @@ const RecipesView = ({route, navigation}: RecipesViewProps) => {
       navigation.navigate('RecipeDetailView', {recipe: recipe});
     };
   }
+  async function performSearch() {
+    if (search.length == 0) {
+      setMessage('Search for a recipe to get started!');
+      setRecipes([]);
+      return;
+    }
+    Keyboard.dismiss();
+    setIsSearching(true);
+    setRecipes([]);
+    setMessage('');
+    Network.fetchRecipes(search)
+      .then(fetched => {
+        setIsSearching(false);
+        fetched[0].instructions = [];
+        setRecipes(fetched);
+        if (fetched.length == 0) {
+          setMessage(
+            'Could not find any recipes. Please try searching using a different query.',
+          );
+        }
+      })
+      .catch(error => {
+        setIsSearching(false);
+        setMessage(`There was an error when searching for recipes: ${error}`);
+      });
+  }
   return (
     <View style={styles.view}>
-      <TextInput
-        underlineColor="transparent"
-        activeUnderlineColor="transparent"
-        onChangeText={updateSearch}
-        value={search}
-        placeholder="Search for a recipe..."
-        style={styles.searchBar}></TextInput>
-      <FlatList
-        data={sampleRecipes}
-        renderItem={({item}) => RecipeCard(item, getOnClick(item))}
-        keyExtractor={item => item.id.toString()}
-      />
+      {SearchBox(updateSearch, search, performSearch)}
+      {isSearching && <ActivityIndicator />}
+      {message.length > 0 && <Text style={styles.message}>{message}</Text>}
+      {recipes.length > 0 && (
+        <FlatList
+          data={recipes}
+          renderItem={({item}) => RecipeCard(item, getOnClick(item))}
+          keyExtractor={item => item.id.toString()}
+        />
+      )}
     </View>
   );
 };
@@ -95,8 +145,12 @@ const styles = StyleSheet.create({
     fontSize: 14,
     color: 'white',
   },
+  message: {
+    margin: 32,
+    fontSize: 16,
+    textAlign: 'center',
+  },
   searchBar: {
-    backgroundColor: 'white',
     margin: 16,
     height: 50,
     borderWidth: 0,
@@ -104,10 +158,31 @@ const styles = StyleSheet.create({
     borderTopLeftRadius: 25,
     borderTopRightRadius: 25,
     marginBottom: 16,
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  searchInput: {
+    backgroundColor: 'white',
+    flex: 1,
+    borderTopLeftRadius: 25,
+    borderBottomLeftRadius: 25,
+    borderColor: '#5dbb63',
+    borderWidth: 2,
+    paddingHorizontal: 16,
+  },
+  searchIcon: {
+    paddingHorizontal: 8,
+    paddingRight: 12,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#5dbb63',
+    borderTopRightRadius: 25,
+    borderBottomRightRadius: 25,
   },
   view: {
-    height: '100%'
-  }
+    height: '100%',
+  },
 });
 
 export default RecipesView;
